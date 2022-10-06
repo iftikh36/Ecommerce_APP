59b8fa8446b02ad1c197ed8d33547939ae19fd24
iftikhar <ifti.wazir36@gmail.com>
2022-10-07 00:42:27 +0530

    Day-10 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/catalogs.dart';
 import 'package:flutter_application_1/pages/widgets/drawer.dart';
 
 import 'widgets/items_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Codepur";
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Codepur";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogsJson =
+        await rootBundle.loadString("assets/files/catalogs.json");
+    final decodedData = jsonDecode(catalogsJson);
+    var productsData = decodedData["product"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -21,10 +45,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: dummyList.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidget(
-              item: dummyList[index],
+              item: CatalogModel.items[index],
             );
           },
         ),
